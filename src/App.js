c10fb3a1000cd9b554b48771c5aa5baa9a6f0b2a
Stephen Fox <stevethecoder34@gmail.com>
2017-07-07 09:04:57 -0600

    Initial commit with fancy hello world
    Assisted by this tutorial:
      1) https://medium.com/@diamondgfx/learning-react-with-create-react-app-part-1-a12e1833fdc
      2) https://medium.com/@diamondgfx/learning-react-with-create-react-app-part-2-3ad99f38b48d
      3) https://medium.com/@diamondgfx/learning-react-with-create-react-app-part-3-322447d14192

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './hello-world/HelloWorld';
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld name="Jane" />
+    </div>
+  )
+}
+
+export default App;
